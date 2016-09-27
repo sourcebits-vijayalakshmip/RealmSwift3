cea5523a42787866ca4552a4cdc123a022493bc9
vijayalakshmi <vijayalakshmi.pulivarthi@sourcebits.com>
2016-09-27 11:39:36 +0530

    Initial Commit

---
+++ RealmSwift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmSwift3
+//
+//  Created by Vijayalakshmi Pulivarthi on 27/09/16.
+//  Copyright © 2016 sourcebits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
