a25312acf72d0274ab5c674bf29a510208481c04
vijayalakshmi <vijayalakshmi.pulivarthi@sourcebits.com>
2016-09-27 12:01:07 +0530

    API is added

--- RealmSwift3/ViewController.swift
+++ RealmSwift3/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    var jsonDict: NSDictionary = [:]
+    var jsonArray: NSArray = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.callAPIForAlbums()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func callAPIForAlbums() -> Void {
+        
+        // The URL request sent to the server.
+        let urlPath = "https://itunes.apple.com/us/rss/topalbums/limit=10/json"
+        Alamofire.request(urlPath, method: .get, parameters: nil).responseJSON() { response in
+            
+            //print(response)
+            
+            if let JSON = response.result.value {
+                self.jsonDict = (JSON as? NSDictionary)!
+                let jsonFeed = self.jsonDict["feed"] as? NSDictionary
+                let jsonAuthor = jsonFeed?["author"] as? NSDictionary
+                
+                if let jsonName = jsonAuthor?["name"] as? NSDictionary {
+                    let name = jsonName["label"] as! String
+                    print("name..", name)
+                    
+                }
+                if let jsonUrl = jsonAuthor?["uri"] as? NSDictionary {
+                    let url = jsonUrl["label"] as! String
+                    print("url..", url)
+                }
+                
+                if let jsonEntries = jsonFeed?["entry"] as? NSArray {
+                    print("entry..", jsonEntries.count)
+                    
+                    for elements in jsonEntries {
+                        print("elements..", elements)
+                    }
+                }
+            }
+        }
+        
+    }
 
 }
 
